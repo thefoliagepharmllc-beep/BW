e9b74a67720e9d70d6c7d87fd423babcd50ece9d
thefoliagepharmllc-beep <thefoliagepharmllc@gmail.com>
2026-04-21 08:52:29 -0400

    Modify PWA settings in next.config.js
    Updated PWA configuration to enable registration and skip waiting.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
 }); 
-Bash
-npm install next-pwa
-Bash
-git add .
-  git commit -m "added next-pwa"
-git push
